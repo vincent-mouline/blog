652410babb98cd21fb24d92af65ab66b92d8c9ef
Vinny <paulvinny@gmail.com>
2019-05-13 15:45:36 +0200

    add category in article bdd by doctrine

--- src/Entity/Article.php
+++ src/Entity/Article.php
@@ -26,6 +26,12 @@ class Article
      */
     private $content;
 
+    /**
+     * @ORM\ManyToOne(targetEntity="App\Entity\category")
+     * @ORM\JoinColumn(nullable=false)
+     */
+    private $category;
+
     public function getId(): ?int
     {
         return $this->id;
@@ -51,6 +57,18 @@ class Article
     public function setContent(string $content): self
     {
         $this->content = $content;
+
+        return $this;
+    }
+
+    public function getCategory(): ?category
+    {
+        return $this->category;
+    }
+
+    public function setCategory(?category $category): self
+    {
+        $this->category = $category;
 
         return $this;
     }
