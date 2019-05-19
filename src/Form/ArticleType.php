442e7396a912d78f96d0578472a8fb067451209c
Vinny <paulvinny@gmail.com>
2019-05-19 20:31:29 +0200

    add tag selector for article edit

--- src/Form/ArticleType.php
+++ src/Form/ArticleType.php
@@ -3,6 +3,8 @@
 namespace App\Form;
 
 use App\Entity\Article;
+use App\Entity\Tag;
+use Symfony\Bridge\Doctrine\Form\Type\EntityType;
 use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
@@ -15,7 +17,14 @@ class ArticleType extends AbstractType
             ->add('title')
             ->add('content')
             ->add('category', null, ['choice_label' => 'name'])
-        ;
+            ->add('tags', EntityType::class, [
+                    'class' => Tag::class,
+                    'choice_label' => 'name',
+                    'multiple' => true,
+                    'expanded' => true,
+                    'by_reference' => false,
+                ]
+            );
     }
 
     public function configureOptions(OptionsResolver $resolver)
